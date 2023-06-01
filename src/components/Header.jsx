726c83da401ea2d64af354272670e96e979bde07
delficasarino <delficasarino@gmail.com>
2023-06-01 12:21:11 -0300

    Header setSelected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { useContext } from "react";
 import SearchContext from "../context/SearchContext";
+import SelectedContext from "../context/SelectedContext";
+
 import "../scss/header.scss";
 
 function Header() {
   const { setSearch } = useContext(SearchContext);
+  const { setSelected } = useContext(SelectedContext);
   const submit = () => {
     setSearch("");
+    setSelected(null);
   };
   return (
     <header className="df rw wp jcc aic header">
