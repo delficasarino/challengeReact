e1502a42d9a686d39cad40fb2e86f56f2c7b185c
delficasarino <delficasarino@gmail.com>
2023-06-01 11:58:58 -0300

    header reset search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
+import SearchContext from "../context/SearchContext";
 import "../scss/header.scss";
 
 function Header() {
+  const { setSearch } = useContext(SearchContext);
+  const submit = () => {
+    setSearch("");
+  };
   return (
-    <header className="df rw wp jcc aic">
-      <h1>SEARCH BOOKS</h1>
+    <header className="df rw wp jcc aic header">
+      <span className="header__title" onClick={submit}>
+        SEARCH BOOKS
+      </span>
     </header>
   );
 }
