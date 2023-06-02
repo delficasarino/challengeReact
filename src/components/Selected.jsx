c20e2e9d677a3e08de720ba4c6dcdaa2a8f23324
delficasarino <delficasarino@gmail.com>
2023-06-02 11:07:06 -0300

    style selected book update

--- src/components/Selected.jsx
+++ src/components/Selected.jsx
@@ -9,35 +9,40 @@ const Selected = () => {
     <>
       {selected && (
         <ul className="book-selected">
-          <FaWindowClose onClick={() => setSelected(null)} className="close" />
-          {selected?.work?.covers && (
-            <img
-              src={`https://covers.openlibrary.org/b/id/${selected?.work?.covers[0]}-L.jpg`}
-              alt="cover"
-              className="book-selected__image"
+          <section className="book-selected__section">
+            <FaWindowClose
+              onClick={() => setSelected(null)}
+              className="close"
             />
-          )}
-          <article className="book-selected__text">
-            <li className="book-selected__title">{selected?.book?.title}</li>
-            <li className="book-selected__first-publish-year">
-              First Publish Year: {selected?.book?.first_publish_year}
-            </li>
-            {selected?.work?.description && (
-              <li className="book-selected__description">
-                {selected?.work?.description?.value ||
-                  selected?.work?.description?.value ||
-                  selected?.work?.description}
-              </li>
+            {selected?.work?.covers && (
+              <img
+                src={`https://covers.openlibrary.org/b/id/${selected?.work?.covers[0]}-L.jpg`}
+                alt="cover"
+                className="book-selected__image"
+              />
             )}
-            <li>Author: {selected?.book?.author_name}</li>
-            {selected?.author?.bio && (
-              <li>
-                {selected?.author?.bio?.value ||
-                  selected?.author?.bio?.value ||
-                  selected?.author?.bio}
+            <article className="book-selected__text">
+              <li className="book-selected__title">{selected?.book?.title}</li>
+              <li className="book-selected__first-publish-year">
+                First Publish Year: {selected?.book?.first_publish_year}
               </li>
-            )}
-          </article>
+              {selected?.work?.description && (
+                <li className="book-selected__description">
+                  {selected?.work?.description?.value ||
+                    selected?.work?.description?.value ||
+                    selected?.work?.description}
+                </li>
+              )}
+              <li>Author: {selected?.book?.author_name}</li>
+              {selected?.author?.bio && (
+                <li>
+                  {selected?.author?.bio?.value ||
+                    selected?.author?.bio?.value ||
+                    selected?.author?.bio}
+                </li>
+              )}
+            </article>
+          </section>
         </ul>
       )}
     </>
